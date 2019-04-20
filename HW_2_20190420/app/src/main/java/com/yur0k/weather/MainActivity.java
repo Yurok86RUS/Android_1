348494dc8f567d9cf588eb6bd5bdc114723739b4
Yurok86RUS <yurok.kolesnikov@gmail.com>
2019-04-20 22:39:15 +0500

    finish task 1

--- HW_2_20190420/app/src/main/java/com/yur0k/weather/MainActivity.java
+++ HW_2_20190420/app/src/main/java/com/yur0k/weather/MainActivity.java
@@ -2,12 +2,54 @@ package com.yur0k.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Logs("onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Logs("onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Logs("onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Logs("onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Logs("onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Logs("onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Logs("onDestroy");
+        super.onDestroy();
+    }
+
+    private void Logs (String msg){
+        Log.d("Method", msg);
     }
 }
