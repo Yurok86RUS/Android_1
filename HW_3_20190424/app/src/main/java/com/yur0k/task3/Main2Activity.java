3e9eb5b680f7afeb21a68805215cffb034b17050
Yurok86RUS <yurok.kolesnikov@gmail.com>
2019-04-25 23:13:16 +0500

    finish task 3

--- HW_3_20190424/app/src/main/java/com/yur0k/task3/Main2Activity.java
+++ HW_3_20190424/app/src/main/java/com/yur0k/task3/Main2Activity.java
@@ -1,7 +1,13 @@
 package com.yur0k.task3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static android.provider.Telephony.Mms.Part.TEXT;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        String text = getIntent().getExtras().getString(TEXT);
+        TextView textView = (TextView) findViewById(R.id.text_city_Name);
+        textView.setText(text);
+        Button button = (Button) findViewById(R.id.button_Back);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- HW_3_20190424/app/src/main/java/com/yur0k/task3/MainActivity.java
+++ HW_3_20190424/app/src/main/java/com/yur0k/task3/MainActivity.java
@@ -5,23 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import static android.provider.Telephony.Mms.Part.TEXT;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void onClick(View v){
-        switch (v.getId()){
-            case R.id.button_OK:
-                Intent intent = new Intent(this, Main2Activity.class);
+        Button button = (Button) findViewById(R.id.button_OK);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.city_Name);
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra(TEXT, editText.getText().toString());
                 startActivity(intent);
-                break;
-            default:
-                break;
-        }
+            }
+        });
     }
 }
