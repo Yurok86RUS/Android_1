703b1bb4fc10907adb097d105d10df79220bd28b
Yurok <yurok.kolesnikov@gmail.com>
2019-05-12 11:40:34 +0500

    copy project

---
+++ HW_6_20190512/app/src/main/java/com/yur0k/weather/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yur0k.weather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+//Главная активити
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
